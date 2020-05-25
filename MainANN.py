70764705f02b64fbb21ff87d5545bf305527ce84
roffogiorgio <roffo.giorgio@gmail.com>
2020-05-25 09:57:16 +0100

    updates

--- MainANN.py
+++ MainANN.py
@@ -12,7 +12,7 @@ from sklearn.metrics import confusion_matrix
 from sklearn.compose import ColumnTransformer 
 
 # Importing the dataset
-dataset = pd.read_csv('Churn_Modelling.csv')
+dataset = pd.read_csv('dataset.csv')
 X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
 
